84a935c26d1eadef8dc5628154f832d9a10ee5e2
tametsi <93092155+tametsi@users.noreply.github.com>
2022-04-18 11:20:34 +0200

    Add fade-in transition for page switch

--- src/components/pages/BasePage.svelte
+++ src/components/pages/BasePage.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+	import { fade } from 'svelte/transition';
 	import activePage, { Page } from '../../stores/activePage';
 
 	export let page: Page;
 </script>
 
 {#if $activePage === page}
-	<main>
+	<main in:fade={{ duration: 200 }}>
 		<slot />
 	</main>
 {/if}
