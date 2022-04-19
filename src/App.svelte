b28180172f03b249295e1ba63f21587c602737f6
tametsi <93092155+tametsi@users.noreply.github.com>
2022-04-19 16:21:37 +0200

    Add Sidebar to navigate easy

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,35 @@
 <script lang="ts">
 	import Pages from './components/Pages.svelte';
+	import Sidebar from './components/Sidebar.svelte';
 </script>
 
 <div class="wrapper">
+	<aside class="sidebar">
+		<Sidebar />
+	</aside>
+
 	<div class="pages">
 		<Pages />
 	</div>
 </div>
+
+<style lang="scss">
+	.wrapper {
+		display: grid;
+		grid-template-areas:
+			'side main';
+		grid-template-columns: clamp(50px, 18rem, 10%) 1fr;
+		grid-template-rows: 1fr;
+
+		height: 100vh;
+		overflow-y: hidden;
+
+		.sidebar {
+			grid-area: side;
+			overflow-y: auto;
+		}
+		.pages {
+			grid-area: main;
+		}
+	}
+</style>
---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,62 @@
+<script>
+	import activePage, { Page } from '../stores/activePage';
+</script>
+
+<ul>
+	<div>
+		<li>
+			<a href="#main" class:active={$activePage === Page.main}>
+				🏃 Main
+			</a>
+		</li>
+	</div>
+	<div>
+		<li>
+			<a href="#about" class:active={$activePage === Page.about}>
+				🐸 About
+			</a>
+		</li>
+	</div>
+</ul>
+
+<style lang="scss">
+	ul {
+		list-style: none;
+
+		display: flex;
+		justify-content: space-between;
+		flex-flow: column nowrap;
+		height: 100%;
+		width: 100%;
+
+		background-color: var(--clr-bg2);
+
+		border-right: 0.3rem solid var(--clr-accent);
+
+		li a {
+			$margin: 0.4rem;
+
+			display: block;
+			color: inherit;
+			width: calc(100% - $margin * 2);
+			height: 100%;
+
+			padding: 0.5rem;
+			margin: $margin;
+			border-radius: 0.2rem;
+
+			transition: all 0.2s;
+			&:hover {
+				background-color: var(--clr-bg3);
+			}
+			&:active {
+				letter-spacing: -0.1rem;
+			}
+
+			&.active {
+				background-color: var(--clr-bg3);
+				color: var(--clr-accent-light);
+			}
+		}
+	}
+</style>
