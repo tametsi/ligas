0d9777e7efc97e2821ea4229196a74559cef3547
tametsi <93092155+tametsi@users.noreply.github.com>
2022-04-21 22:01:21 +0200

    Add runners on main page

--- src/components/pages/Main.svelte
+++ src/components/pages/Main.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import { Page } from '../../stores/activePage';
+	import activeRun from '../../stores/activeRun';
+	import Runner from '../Runner.svelte';
 	import BasePage from './BasePage.svelte';
 </script>
 
 <BasePage page={Page.main}>
+	<div class="runners">
+		{#each $activeRun.runners as runner}
+			<Runner {runner} />
+		{/each}
+	</div>
 </BasePage>
+
+<style lang="scss">
+	.runners {
+		display: flex;
+		justify-content: space-around;
+		flex-flow: row wrap;
+	}
+</style>
