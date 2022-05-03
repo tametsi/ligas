692efed4024c6678fe577d41f10cce2a2e668cee
tametsi <93092155+tametsi@users.noreply.github.com>
2022-05-03 12:33:10 +0200

    Add timer-store

---
+++ src/stores/activeTimer.ts
@@ -0,0 +1,19 @@
+import { writable } from 'svelte/store';
+import Timer from '../lib/timer';
+
+function createStore() {
+	const { set, subscribe, update } = writable(new Timer());
+
+	return {
+		set,
+		subscribe,
+		updateSelf: (updater: (x: Timer) => void) => {
+			update(x => {
+				updater(x);
+				return x;
+			});
+		},
+	};
+}
+
+export default createStore();
