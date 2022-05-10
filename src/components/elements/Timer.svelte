d9953c3b8a340f194eb45c8cb2207326a2c153a0
tametsi <93092155+tametsi@users.noreply.github.com>
2022-05-10 12:55:25 +0200

    Add Timer display component, handling `Timer` updates

---
+++ src/components/elements/Timer.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { TimerState } from '../../lib/timer';
+	import activeTimer from '../../stores/activeTimer';
+
+	let time: string;
+	let interval: number;
+
+	activeTimer.subscribe(x => {
+		function updateTime() {
+			// TODO time formatting
+			time = x.getRunDuration().toString();
+		}
+
+		if (x.state === TimerState.running && interval === undefined)
+			interval = setInterval(
+				() => {
+					updateTime();
+				},
+				100,
+				undefined
+			);
+		else if (x.state === TimerState.stopped && interval >= 0) {
+			clearInterval(interval);
+			interval = undefined;
+			updateTime();
+		}
+	});
+</script>
+
+<span>{time}</span>
