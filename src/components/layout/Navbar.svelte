29588a09685992de7c412abd699accb81d9936ed
tametsi <93092155+tametsi@users.noreply.github.com>
2022-05-10 13:04:35 +0200

    Add timer-toggle button & `Timer`-component in navbar

--- src/components/layout/Navbar.svelte
+++ src/components/layout/Navbar.svelte
@@ -1,4 +1,17 @@
+<script lang="ts">
+	import activeTimer from '../../stores/activeTimer';
+	import Timer from '../elements/Timer.svelte';
+
+	function toggleTimer() {
+		activeTimer.updateSelf(x => x.toggle());
+	}
+</script>
+
 <h1>LIGAS</h1>
+
+<button on:click={toggleTimer}>Toggle Timer</button>
+
+<Timer />
 
 <style lang="scss">
 	@use '../../scss/abstracts' as a;
