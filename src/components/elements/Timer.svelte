919940489f8b4afe4e44cd76a5afae0032956d98
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2022-05-11 16:08:10 +0200

    Add time formatting
    Timer component now displays time in the format 'mm:ss:ff' instead of raw milliseconds.

--- src/components/elements/Timer.svelte
+++ src/components/elements/Timer.svelte
@@ -6,17 +6,24 @@
 	let interval: number;
 
 	activeTimer.subscribe(x => {
-		const updateTime = () => {
-			// TODO time formatting
-			time = x.getRunDuration().toString();
-		}
+		const padTime = (time: number) => time.toString().padStart(2, '0');
+
+		const formatTime = (ms: number) => {
+			if (isNaN(ms)) return '00:00:00';
+
+			const date = new Date(ms);
+			const minutes = padTime(date.getUTCMinutes());
+			const seconds = padTime(date.getUTCSeconds());
+			const centis = padTime(Math.floor(date.getUTCMilliseconds() / 10));
+
+			return `${minutes}:${seconds}:${centis}`;
+		};
+
+		const updateTime = () => (time = formatTime(x.getRunDuration()));
+		updateTime();
 
 		if (x.state === TimerState.running && interval === undefined)
-			interval = setInterval(
-				updateTime,
-				100,
-				undefined
-			);
+			interval = setInterval(updateTime, 100, undefined);
 		else if (x.state === TimerState.stopped && interval >= 0) {
 			clearInterval(interval);
 			interval = undefined;
