ea5bc6e968482280c918a862d90ee8ac91c12f65
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2022-05-12 09:54:37 +0200

    Refactor: Move time formatting into seperate file

--- src/components/elements/Timer.svelte
+++ src/components/elements/Timer.svelte
@@ -1,24 +1,12 @@
 <script lang="ts">
 	import { TimerState } from '../../lib/timer';
+	import { formatTime } from '../../lib/formatter';
 	import activeTimer from '../../stores/activeTimer';
 
 	let time: string;
 	let interval: number;
 
 	activeTimer.subscribe(x => {
-		const padTime = (time: number) => time.toString().padStart(2, '0');
-
-		const formatTime = (ms: number) => {
-			if (isNaN(ms)) return '00:00:00';
-
-			const date = new Date(ms);
-			const minutes = padTime(date.getUTCMinutes());
-			const seconds = padTime(date.getUTCSeconds());
-			const centis = padTime(Math.floor(date.getUTCMilliseconds() / 10));
-
-			return `${minutes}:${seconds}:${centis}`;
-		};
-
 		const updateTime = () => (time = formatTime(x.getRunDuration()));
 		updateTime();
 
