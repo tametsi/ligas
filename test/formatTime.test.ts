063914fdb3f3c9d4c9c0f8d082aced5ed21d1a66
tametsi <93092155+tametsi@users.noreply.github.com>
2022-05-12 11:25:11 +0200

    Add tests for `formatTime`

---
+++ test/formatTime.test.ts
@@ -0,0 +1,19 @@
+import formatTime from '../src/lib/formatTime';
+
+describe('formatTime', () => {
+	it('formatting', () => {
+		const defaultTime = '00:00:00';
+
+		expect(formatTime(NaN)).toBe(defaultTime);
+		expect(formatTime(null)).toBe(defaultTime);
+		expect(formatTime(undefined)).toBe(defaultTime);
+
+		expect(formatTime(0)).toBe(defaultTime);
+		expect(formatTime(110)).toBe('00:00:11');
+		expect(formatTime(610)).toBe('00:00:61');
+		expect(formatTime(6610)).toBe('00:06:61');
+		expect(formatTime(61000)).toBe('01:01:00');
+		expect(formatTime(60000 * 60 - 10)).toBe('59:59:99');
+		expect(formatTime(60000 * 60)).toBe('00:00:00');
+	});
+});
