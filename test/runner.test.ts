fe50374827cc7e942a176df140fe453c1e9bfb05
tametsi <93092155+tametsi@users.noreply.github.com>
2022-05-17 10:24:07 +0200

    Create `RoundManager` tests

---
+++ test/runner.test.ts
@@ -0,0 +1,42 @@
+import Runner from '../src/lib/runner';
+
+describe('Runner', () => {
+	describe('RoundManager', () => {
+		let runner: Runner;
+
+		beforeEach(() => {
+			runner = new Runner('1', 'name');
+		});
+
+		it('init', () => {
+			expect(runner.rounds.all).toEqual([]);
+			expect(runner.rounds.last).toBe(undefined);
+			expect(runner.rounds.count).toBe(0);
+			expect(runner.rounds.totalTime).toBe(0);
+		});
+
+		it('adding rounds', () => {
+			expect(runner.rounds.all).toEqual([]);
+
+			runner.rounds.add(10);
+			runner.rounds.add(15);
+
+			expect(runner.rounds.all).toEqual([10, 15]);
+			expect(runner.rounds.last).toBe(15);
+			expect(runner.rounds.count).toBe(2);
+			expect(runner.rounds.totalTime).toBe(25);
+		});
+
+		it('adding rounds by time', () => {
+			expect(runner.rounds.all).toEqual([]);
+
+			runner.rounds.addByTime(10);
+			runner.rounds.addByTime(25);
+
+			expect(runner.rounds.all).toEqual([10, 15]);
+			expect(runner.rounds.last).toBe(15);
+			expect(runner.rounds.count).toBe(2);
+			expect(runner.rounds.totalTime).toBe(25);
+		});
+	});
+});
