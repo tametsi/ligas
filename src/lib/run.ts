52a0ea932b90af68b1825c165e894f96c45b0784
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2022-05-18 19:18:47 +0200

    Refactoring: Improve readability of 'Run'

--- src/lib/run.ts
+++ src/lib/run.ts
@@ -1,20 +1,21 @@
 import Runner from './runner';
 
 export default class Run {
-	private _roundLength = 400;
 	private _runners: Map<string, Runner> = new Map();
 
-	get roundLength() {
-		return this._roundLength;
-	}
-	set roundLength(x) {
-		this._roundLength = x;
-	}
+	/** The distance of a single round in this run (in meters) */
+	public roundLength = 400;
 
+	/** All the runners currently being part of this run */
 	get runners() {
 		return [...this._runners.values()];
 	}
 
+	/**
+	 * Adds a runner with the given Information to this run
+	 * @param name The name of the new runner
+	 * @param alias The alias of the new runner
+	 */
 	addRunner(name: string, alias?: string) {
 		let id = this._runners.size;
 
