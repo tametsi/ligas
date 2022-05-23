f351fcc5bfd449161beb29b8754e80330ad07f23
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2022-05-23 09:50:48 +0200

    Add basic functionality to `Export` page

--- src/components/pages/Export.svelte
+++ src/components/pages/Export.svelte
@@ -1,6 +1,33 @@
 <script lang="ts">
+	import activeRun from '../../stores/activeRun';
 	import { Page } from '../../stores/activePage';
 	import BasePage from './BasePage.svelte';
+	import { downloadCsv } from '../../lib/util/downloadFile';
+
+	function exportData() {
+		const header = [
+			'Name',
+			'Alias',
+			'Rounds',
+			'Distance (in m)',
+			'Max. Difference (in s)',
+			'Rounds (Format: mm:ss:ff)',
+		];
+		const stats = $activeRun.runners.map(runner => runner.stats);
+
+		const data = [header, ...stats];
+		downloadCsv(data, { filename: 'run.csv', seperator: ';\t' });
+	}
 </script>
 
-<BasePage page={Page.export} />
+<BasePage page={Page.export}>
+	<form on:submit|preventDefault={exportData}>
+		<figure>
+			<figcaption>Export data</figcaption>
+
+			<div class="form-controls">
+				<button type="submit" class="button">Export</button>
+			</div>
+		</figure>
+	</form>
+</BasePage>
