d1e3f6767cbf9ee26363e82e49a8995d146d27b6
tametsi <93092155+tametsi@users.noreply.github.com>
2022-07-31 21:10:42 +0200

    Add: take color theme preference into account

--- src/components/elements/ThemePicker.svelte
+++ src/components/elements/ThemePicker.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import activeTheme, { Theme } from '@stores/activeTheme';
+	import { onMount } from 'svelte';
 	import { SunIcon, MoonIcon } from 'svelte-feather-icons';
+
+	onMount(() => {
+		// change theme if user prefers light color scheme
+		if (window.matchMedia('(prefers-color-scheme: light)').matches)
+			$activeTheme = Theme.light;
+	});
 
 	const toggleTheme = () =>
 		activeTheme.update(x => (x === Theme.dark ? Theme.light : Theme.dark));
