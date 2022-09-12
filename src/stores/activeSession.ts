f2525c0cea5fecba5f62c94c4b37157a1c8a8a9f
tametsi <93092155+tametsi@users.noreply.github.com>
2022-09-12 20:39:03 +0200

    refactor: create session-store with `createWriteableObjectStore`

--- src/stores/activeSession.ts
+++ src/stores/activeSession.ts
@@ -1,24 +1,11 @@
-import { get, writable } from 'svelte/store';
+import { get } from 'svelte/store';
+import { createWriteableObjectStore } from '@stores/createStore';
 import Timer from '@lib/timer';
 import Run from '@lib/run';
 import Session from '@lib/session';
 
-function createStore() {
-	const { set, subscribe, update } = writable(
-		new Session(new Timer(), new Run())
-	);
-
-	return {
-		set,
-		subscribe,
-		updateSelf: (updater: (x: Session) => void) => {
-			update(x => {
-				updater(x);
-				return x;
-			});
-		},
-	};
-}
-const activeSession = createStore();
+const activeSession = createWriteableObjectStore(
+	new Session(new Timer(), new Run())
+);
 window.addEventListener('beforeunload', () => get(activeSession).save());
 export default activeSession;
