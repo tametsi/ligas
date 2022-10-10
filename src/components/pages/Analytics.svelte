a581c30e84bba53bc9f194106118b181bd21e027
Niklas <37415436+Fini04@users.noreply.github.com>
2022-10-10 18:15:37 +0200

    feat: add analytics

--- src/components/pages/Analytics.svelte
+++ src/components/pages/Analytics.svelte
@@ -3,98 +3,121 @@
 	import BasePage from '@components/pages/BasePage.svelte';
 	import { onMount } from 'svelte';
 	import activeRun from '@stores/activeRun';
+	import FormItem from '@components/elements/FormItem.svelte';
+	import type Runner from '@lib/runner';
 
-	let c: any = document.getElementById('canvas');
-	let ctx: any;
-	let testData = [100, 250, 200, 50, 150];
-	let selectedRunner;
-	let runnerArray = $activeRun.runners;
-
-	let xIntervallLenght;
+	let c: HTMLCanvasElement;
+	let ctx: CanvasRenderingContext2D;
+	let selectedRunner: Runner;
+	let xIntervallLenght: number, yCanvasHeight: number;
 
 	function drawCurve() {
-		for (let index = 0; index < testData.length; index++) {
-			ctx.moveTo(index * xIntervallLenght, testData[index]);
-			ctx.lineTo((index + 1) * xIntervallLenght, testData[index + 1]);
-		}
+		ctx.beginPath();
+		selectedRunner.rounds.all.forEach((value, index) => {
+			ctx.moveTo(
+				index * xIntervallLenght,
+				yCanvasHeight -
+					(selectedRunner.rounds.all[index] /
+						Math.max(...selectedRunner.rounds.all)) *
+						yCanvasHeight
+			);
+			ctx.lineTo(
+				(index + 1) * xIntervallLenght,
+				yCanvasHeight -
+					(selectedRunner.rounds.all[index + 1] /
+						Math.max(...selectedRunner.rounds.all)) *
+						yCanvasHeight
+			);
+		});
+		ctx.stroke();
+		ctx.fill();
 	}
 
 	function canvasSetup() {
-		if (runnerArray.length == 0) {
-			xIntervallLenght = 0;
-		} else {
-			xIntervallLenght = c.width / runnerArray.length;
-		}
 		ctx = c.getContext('2d');
 		ctx.strokeStyle = '#1ca8b6';
-		drawCurve();
+		ctx.lineWidth = 3;
+		xIntervallLenght = c.width / (selectedRunner.rounds.all.length - 1);
+		yCanvasHeight = c.height;
 
-		ctx.stroke();
-		ctx.fill();
-		logRunners();
+		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		drawCurve();
 	}
 
 	onMount(() => {
 		canvasSetup();
-		// ctx.moveTo(0, 0);
-		// ctx.lineTo(600, 100);
-		// clearCanvas();
 	});
-	function logRunners() {
-		runnerArray = $activeRun.runners;
-		console.log(runnerArray);
-		for (let index = 0; index < runnerArray.length; index++) {
-			if (runnerArray[index].id == runnerId) {
-				selectedRunner = runnerArray[index];
-				console.log(selectedRunner);
-			}
-		}
-	}
-
-	function getRoundTime() {
-		return 'test';
-	}
-
-	function clearCanvas() {
-		ctx.beginPath();
-		ctx.fillStyle = '#3b3e45';
-		ctx.rect(0, 0, ctx.canvas.width, ctx.canvas.height);
-		ctx.fill();
-	}
-	let runnerId;
 </script>
 
 <BasePage page={Page.analytics}>
-	<h2>Analytics</h2>
+	<form on:submit|preventDefault>
+		<figure>
+			<figcaption>Stats</figcaption>
+			<FormItem name="Select Runner">
+				<select
+					id="runers"
+					bind:value={selectedRunner}
+					on:change={canvasSetup}
+				>
+					{#each $activeRun.runners as runner}
+						<option value={runner}>{runner.name}</option>
+					{/each}
+				</select>
+			</FormItem>
+		</figure>
+	</form>
 
-	<label for="runners">Select Runner</label>
-	<select id="runers" bind:value={runnerId} on:change={() => logRunners()}>
-		{#each $activeRun.runners as runner}
-			<option value={runner.id}>{runner.name}</option>
-		{/each}
-	</select>
+	<table class="thin">
+		<tbody>
+			<tr>
+				<td>Name</td>
+				<td>{selectedRunner?.name ?? '-'}</td>
+			</tr>
+			<tr>
+				<td>Alias</td>
+				<td>{selectedRunner?.alias ?? '-'}</td>
+			</tr>
+			<tr>
+				<td>Rounds</td>
+				<td>{selectedRunner?.rounds?.all?.length ?? '-'}</td>
+			</tr>
+			<tr>
+				<td>Distance</td>
+				<td>{selectedRunner?.distance ?? '-'}</td>
+			</tr>
+			<tr>
+				<td>Avarage Roundtime</td>
+				<td
+					>{Math.round(selectedRunner?.rounds?.averageTime ?? 0) /
+						1000}s</td
+				>
+			</tr>
+			<tr>
+				<td>Fastest Round</td>
+				<td
+					>{Math.min(...(selectedRunner?.rounds?.all ?? [0])) /
+						1000}s</td
+				>
+			</tr>
+			<tr>
+				<td>Slowest Round</td>
+				<td
+					>{Math.max(...(selectedRunner?.rounds?.all ?? [0])) /
+						1000}s</td
+				>
+			</tr>
+		</tbody>
+	</table>
 
-	//TODO - Add that the stats of the selected runner are displayed
-	<h3>Stats</h3>
-	{#if selectedRunner != null}
-		<h4>Name: {selectedRunner.name}</h4>
-		<h4>Rounds: {selectedRunner.rounds.all}</h4>
-		<h4>Distance: {selectedRunner.distance}m</h4>
-	{:else}
-		<h4>Name: <i>no name available</i></h4>
-		<h4>Rounds: <i>no rounds available</i></h4>
-		<h4>Distance: <i>no distance available</i></h4>
-	{/if}
-
-	<canvas
-		bind:this={c}
-		id="canvas"
-		width="600"
-		height="300"
-		style="border:1px solid #000000;"
-	/>
-	<button on:click={() => canvasSetup()}>clear</button>
+	<canvas bind:this={c} height="500" width="1000" />
 </BasePage>
 
 <style lang="scss">
+	canvas {
+		height: 50rem;
+		width: 100rem;
+		max-width: 100%;
+		border: 0.2rem solid var(--clr-bg3);
+		margin-top: 1rem;
+	}
 </style>
