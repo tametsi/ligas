ee24c5faaa670b217dd1b27779439e452bc9cef3
Niklas <37415436+Fini04@users.noreply.github.com>
2022-11-23 21:48:56 +0100

    feat: dont show stats if no runner is available

--- src/components/pages/Analytics.svelte
+++ src/components/pages/Analytics.svelte
@@ -7,7 +7,6 @@
 	import Chart from 'svelte-frappe-charts';
 
 	let selectedRunner: Runner;
-	// avoid having an invalid runner (not in run.runners)
 	$: selectedRunner = $activeSession.run.runners
 		.map(x => x.id)
 		.includes(selectedRunner?.id)
@@ -24,64 +23,73 @@
 </script>
 
 <BasePage page={Page.analytics}>
-	<form on:submit|preventDefault>
-		<figure>
-			<figcaption>Analytics</figcaption>
-			<FormItem name="Select Runner">
-				<select bind:value={selectedRunner}>
-					{#each $activeSession.run.runners as runner}
-						<option value={runner}>{runner.name}</option>
-					{/each}
-				</select>
-			</FormItem>
-		</figure>
-	</form>
+	{#if $activeSession.run.runners.length != 0}
+		<form on:submit|preventDefault>
+			<figure>
+				<figcaption>Analytics</figcaption>
+				<FormItem name="Select Runner">
+					<select bind:value={selectedRunner}>
+						{#each $activeSession.run.runners as runner}
+							<option value={runner}>{runner.name}</option>
+						{/each}
+					</select>
+				</FormItem>
+			</figure>
+		</form>
 
-	<table class="thin">
-		<tbody>
-			<tr>
-				<td>Name</td>
-				<td>{selectedRunner?.name ?? '-'}</td>
-			</tr>
-			<tr>
-				<td>Alias</td>
-				<td>{selectedRunner?.alias ?? '-'}</td>
-			</tr>
-			<tr>
-				<td>Rounds</td>
-				<td>{selectedRunner?.rounds?.all?.length ?? '-'}</td>
-			</tr>
-			<tr>
-				<td>Distance</td>
-				<td>{selectedRunner?.distance ?? '-'}</td>
-			</tr>
-			<tr>
-				<td>Avarage Roundtime</td>
-				<td
-					>{Math.round(selectedRunner?.rounds?.averageTime ?? 0) /
-						1000}s</td
-				>
-			</tr>
-			<tr>
-				<td>Fastest Round</td>
-				<td
-					>{selectedRunner?.rounds?.all.length !== 0
-						? Math.min(...(selectedRunner?.rounds?.all ?? [0])) /
-						  1000
-						: 0}s</td
-				>
-			</tr>
-			<tr>
-				<td>Slowest Round</td>
-				<td>
-					{selectedRunner?.rounds?.all.length !== 0
-						? Math.max(...(selectedRunner?.rounds?.all ?? [0])) /
-						  1000
-						: 0}s</td
-				>
-			</tr>
-		</tbody>
-	</table>
+		<table class="thin">
+			<tbody>
+				<tr>
+					<td>Name</td>
+					<td>{selectedRunner?.name ?? '-'}</td>
+				</tr>
+				<tr>
+					<td>Alias</td>
+					<td>{selectedRunner?.alias ?? '-'}</td>
+				</tr>
+				<tr>
+					<td>Rounds</td>
+					<td>{selectedRunner?.rounds?.all?.length ?? '-'}</td>
+				</tr>
+				<tr>
+					<td>Distance</td>
+					<td>{selectedRunner?.distance ?? '-'}</td>
+				</tr>
+				<tr>
+					<td>Avarage Roundtime</td>
+					<td
+						>{Math.round(selectedRunner?.rounds?.averageTime ?? 0) /
+							1000}s</td
+					>
+				</tr>
+				<tr>
+					<td>Fastest Round</td>
+					<td
+						>{selectedRunner?.rounds?.all.length !== 0
+							? Math.min(
+									...(selectedRunner?.rounds?.all ?? [0])
+							  ) / 1000
+							: 0}s</td
+					>
+				</tr>
+				<tr>
+					<td>Slowest Round</td>
+					<td>
+						{selectedRunner?.rounds?.all.length !== 0
+							? Math.max(
+									...(selectedRunner?.rounds?.all ?? [0])
+							  ) / 1000
+							: 0}s</td
+					>
+				</tr>
+			</tbody>
+		</table>
 
-	<Chart {data} type="line" lineOptions={{ regionFill: 1 }} />
+		<Chart {data} type="line" lineOptions={{ regionFill: 1 }} />
+	{:else}
+		<article>
+			<h2>Analytics</h2>
+			<p>No runners available</p>
+		</article>
+	{/if}
 </BasePage>
