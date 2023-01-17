efcc5af0012c0d059a427e7315ad6c269ba25989
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2023-01-17 11:44:43 +0100

    feat: save settings to local storage

--- src/lib/settings.ts
+++ src/lib/settings.ts
@@ -3,6 +3,31 @@ export enum Theme {
 	light,
 }
 
+const storageId = 'settings';
 export default class Settings {
 	constructor(public theme = Theme.dark) {}
+
+	/** Loads the settings from local storage */
+	static load() {
+		const settings = localStorage.getItem(storageId);
+		if (settings === null) return null;
+		return Settings.fromJSON(JSON.parse(settings));
+	}
+
+	/** Saves the settings in local storage */
+	save() {
+		localStorage.setItem(storageId, JSON.stringify(this));
+	}
+
+	/** Creates new settings from an json-like object */
+	static fromJSON(json: ReturnType<Settings['toJSON']>) {
+		return new Settings(json.theme);
+	}
+
+	/** Converts the settings to a json-like object */
+	toJSON() {
+		return {
+			theme: this.theme,
+		};
+	}
 }
--- src/stores/activeSettings.ts
+++ src/stores/activeSettings.ts
@@ -1,6 +1,10 @@
 import Settings from '@lib/settings';
+import { get } from 'svelte/store';
 import { createWriteableObjectStore } from './createStore';
 export { Theme } from '@lib/settings';
 
-const activeSettings = createWriteableObjectStore(new Settings());
+const activeSettings = createWriteableObjectStore(
+	Settings.load() ?? new Settings()
+);
+window.addEventListener('beforeunload', () => get(activeSettings).save());
 export default activeSettings;
