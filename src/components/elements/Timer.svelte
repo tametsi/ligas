652e43bcd9de969bb57ef1f00406d435cc8ed33a
xXjojaXx <73311246+xXjojaXx@users.noreply.github.com>
2023-04-07 20:07:58 +0200

    refactor: move controls into timer

--- src/components/elements/Timer.svelte
+++ src/components/elements/Timer.svelte
@@ -1,7 +1,28 @@
 <script lang="ts">
 	import formatTime from '@lib/util/formatTime';
 	import activeSession from '@stores/activeSession';
+	import { _ } from '@lib/util/translations';
+	import { PauseIcon, PlayIcon, RotateCcwIcon } from 'svelte-feather-icons';
+
+	$: running = $activeSession.timer.running;
 	$: time = $activeSession.timer.remaining;
+	const reset = () =>
+		activeSession.updateSelf(x => {
+			if (confirm($_('timer.reset_prompt'))) $activeSession.timer.reset();
+		});
 </script>
 
-<span class="align-middle font-mono">{formatTime($time)}</span>
+<button
+	on:click={() => $activeSession.timer.toggle()}
+	class="btn btn-ghost btn-square"
+>
+	<svelte:component this={running ? PauseIcon : PlayIcon} />
+</button>
+
+<p class="px-2 h-12">
+	<span class="align-middle font-mono">{formatTime($time)}</span>
+</p>
+
+<button on:click={reset} class="btn btn-ghost btn-square">
+	<RotateCcwIcon />
+</button>
--- src/components/layout/Navbar.svelte
+++ src/components/layout/Navbar.svelte
@@ -1,18 +1,8 @@
 <script lang="ts">
 	import Timer from '@components/elements/Timer.svelte';
 	import activeSession from '@stores/activeSession';
-	import {
-		MenuIcon,
-		PauseIcon,
-		PlayIcon,
-		RotateCcwIcon,
-	} from 'svelte-feather-icons';
+	import { MenuIcon } from 'svelte-feather-icons';
 	import { _ } from '@lib/util/translations';
-
-	const resetTimer = () =>
-		activeSession.updateSelf(x => {
-			if (confirm($_('timer.reset_prompt'))) x.timer.reset();
-		});
 </script>
 
 <nav class="navbar sticky top-0 z-50 bg-base-300 text-3xl text-justify">
@@ -26,19 +16,6 @@
 	<h1 class="navbar-center h-12 hidden md:flex">LIGAS</h1>
 
 	<div class="navbar-end">
-		<button
-			on:click={() => $activeSession.timer.toggle()}
-			class="btn btn-ghost btn-square"
-		>
-			<svelte:component
-				this={$activeSession.timer.running ? PauseIcon : PlayIcon}
-			/>
-		</button>
-		<p class="px-2 h-12">
-			<Timer />
-		</p>
-		<button on:click={resetTimer} class="btn btn-ghost btn-square">
-			<RotateCcwIcon />
-		</button>
+		<Timer />
 	</div>
 </nav>
